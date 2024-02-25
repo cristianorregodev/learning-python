7866e90808cc49e0570d711d2b83576e80e2bcdb
cristianorregodev <cristian.duquew@gmail.com>
2024-02-25 16:22:00 -0500

    Feat: Add pandas library to the project

--- population-charts/main.py
+++ population-charts/main.py
@@ -1,20 +1,22 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 
 def run():
   data = read_csv.read_csv('data.csv')
+  '''
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
-  
+  '''
 
-  # df = read_csv.read_csv('data.csv')
-  # df = df[df['Continent'] == 'Africa']
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'Africa']
 
-  # countries = df['Country'].values
-  # percentages = df['World Population Percentage'].values
+  countries = df['Country'].values
+  percentages = df['World Population Percentage'].values
   charts.generate_pie_chart(countries, percentages)
 
   
