28b89da0f63dea70ba312ca082563c8f650b10d3
cristianorregodev <cristian.duquew@gmail.com>
2024-02-25 20:27:17 -0500

    Feat: Add a basic web server conf

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,32 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+
+app = FastAPI()
+
+@app.get('/')
+def get_users():
+    return [
+        {'name': 'Cristian Orrego', 'username': 'codev', 'followers': 766},
+        {'name': 'Allan Britos', 'username': 'calibre12', 'followers': 432},
+        {'name': 'Elvert Galarga', 'username': 'elvert', 'followers': 677},
+    ]
+
+
+@app.get('/html', response_class=HTMLResponse)
+def get_html_content():
+    return """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+
 
 def run():
     store.get_store_categories()
